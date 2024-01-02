5752af8ce8858da8084be61cc5fa2753db8290fd
ravikumargandikotavenkata <ravikumar.gandikotavenkata@thomsonreuters.com>
2024-01-02 19:44:23 +0530

    pull from final branch

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,9 @@ plugins {
     id("com.android.application").version("8.1.2").apply(false)
     kotlin("android").version("1.9.20").apply(false)
 }
+
+//tasks {
+//    register("clean", Delete::class) {
+//        delete(rootProject.buildDir)
+//    }
+//}
